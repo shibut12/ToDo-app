0e91582ee4d71c837fd607b44308eb8bf27d7b5b
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-09-08 08:29:18 -0400

    Enabled redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState){
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk)
+        composeEnhancers(
+            applyMiddleware(thunk)
+        )
     );
 }
