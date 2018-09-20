ada002d0e03d3c6906f85a6e59625cca81fca921
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-09-20 00:11:37 -0400

    Updated to get child elements using refs

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -5,49 +5,63 @@ import {connect} from 'react-redux';
 
 let ItemForm =({item, dispatch}) =>{
     let id, description, isCompleted
+    
+    function handleClick(e){
+        console.log('test');
+        const itm={
+            id:id.value,
+            description:description.value,
+            isCompleted:isCompleted.value==='on'?true:false
+        }
+        console.log(itm);
+    };
+
     return(
-        <form onSubmit={e=>{
-            e.preventDefault()
-            const itm={
-                id:id.value,
-                description:description.value,
-                isCompleted:isCompleted.value==='on'?true:false
-            }
-            dispatch(addItem(itm))
-        }}>
-            <div>
-                <label htmlFor="id">Id</label>
+        <div>
+            <form onSubmit={e=>{
+                e.preventDefault()
+                const itm={
+                    id:id.value,
+                    description:description.value,
+                    isCompleted:isCompleted.value==='on'?true:false
+                }
+                dispatch(addItem(itm))
+            }}>
+                <div>
+                    <label htmlFor="id">Id</label>
+                    <input 
+                        name="id"
+                        type="text"
+                        ref={node=>{
+                            id=node
+                        }}/>
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <input 
+                        name="description"
+                        type="text"
+                        ref={node=>{
+                            description = node
+                        }}/>
+                </div>
+                <div>
+                    <label htmlFor="isCompleted">Is Completed?</label>
+                    <input 
+                        name="isCompleted"
+                        type="checkbox"
+                        ref={node=>{
+                            isCompleted = node
+                        }}/>
+                </div>
                 <input 
-                    name="id"
-                    type="text"
-                    ref={node=>{
-                        id=node
-                    }}/>
-            </div>
-            <div>
-                <label htmlFor="description">Description</label>
-                <input 
-                    name="description"
-                    type="text"
-                    ref={node=>{
-                        description = node
-                    }}/>
-            </div>
-            <div>
-                <label htmlFor="isCompleted">Is Completed?</label>
-                <input 
-                    name="isCompleted"
-                    type="checkbox"
-                    ref={node=>{
-                        isCompleted = node
-                    }}/>
-            </div>
-            <input 
-                type="submit"
-                value="Save"/>
-        </form>
+                    type="submit"
+                    value="Save"/>
+            </form>
+            <button onClick={handleClick}>Test</button>
+        </div>
     );
-}
+};
 
 ItemForm.propTypes = {
     item: PropTypes.object
